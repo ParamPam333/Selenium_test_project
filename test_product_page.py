55b17e0eba2bc44ec8991dcb0b4e3702b6982812
Atom-group <zteneoq1@gmail.com>
2024-12-27 12:11:48 +0300

    final edits made

--- test_product_page.py
+++ test_product_page.py
@@ -24,6 +24,7 @@ class TestUserAddToBasketFromProductPage():
         assert page.is_not_element_present(*ProductPageLocators.succ_message), \
         "Success message is presented, but should not be"    
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
         page = ProductPage(browser, link)   
@@ -32,6 +33,7 @@ class TestUserAddToBasketFromProductPage():
         page.solve_quiz_and_get_code()
         page.basket_checked()
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -75,6 +77,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     assert page.is_disappeared(*ProductPageLocators.succ_message), \
     "Success message is presented, but should not be"
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page=BasketPage(browser, link)
@@ -83,4 +86,11 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     assert page.is_not_element_present(*BasketPageLocators.PRODUCT_IN_BASKET), \
        "Product in basket"
     page.basket_is_empty()
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
     
